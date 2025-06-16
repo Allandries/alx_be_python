4361f09015c97361e835a6ed503c3c1da4be9e8c
Allan Alex Mafabi <allanmafabi123@gmail.com>
2025-06-16 13:00:17 +0300

    Simple Interest Calculator in Python

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,12 @@
+'''
+Simple Interest Calculator
+Define all variables
+Input interest formular
+calculate the interest
+'''
+#Step 1: Define Variables
+principal = 1000
+rate = 0.05
+time = 3
+interest = principal * rate * time
+print("The simple interest is:", interest)
