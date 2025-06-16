3289ae41fd8b51f71abf0593081e74e534ea8eae
Allan Alex Mafabi <allanmafabi123@gmail.com>
2025-06-16 13:02:44 +0300

    Created a User Input Age Calculator

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,10 @@
+'''
+User Input Age Calculator
+1. Define variables and their type
+2. Input formular
+3. print results
+'''
+# Step 1: Define variables and their type
+current_age = int(input("How old are you? "))
+future_age = current_age + (2050 - 2023)
+print("In 2050, you will be ", future_age, "years old.")
